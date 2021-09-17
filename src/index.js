9c846515f2c9c87691359e59279c1f5f462c6fc6
USER <user@USERs-MacBook-Air.local>
2021-09-17 17:15:24 -0500

    Webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
+console.log('Gika')
+
+(async function App() {
+  const main = null || document.getElementById('main');
+  main.innerHTML = await Template();
+})();
+
+
